d1e374b2d41339a667d83bbdd7faf1a88347fa32
Alex <522892542@qq.com>
2022-05-15 15:36:51 +0800

    updated

--- backend/apps/Movies/views.py
+++ backend/apps/Movies/views.py
@@ -1,8 +1,45 @@
+# Create your views here.
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
-# Create your views here.
+from conda_build.api import render
+from apps import globalvar as gl
 
 def index(request):
     return HttpResponse( "Hello World" )
+
+
+def recommendation(request):
+    print('recommendation')
+    dataset = gl.get_value( 'dataset' )
+    knn_model = gl.get_value( 'knn_model' )
+    movie_list = list( dataset.index )
+
+    if request.method == 'POST':
+        concat = request.POST
+        postBody = request.body
+        print( concat )
+        print( type( postBody ) )
+        print( postBody )
+        json_result = json.loads( postBody )
+        print( json_result )
+        name = json_result['name']
+        print(name)
+        if name not in movie_list:
+            return HttpResponse( 'Movie [' + str(name) + '] is not in our dataset, So we cannot recommendation' )
+        query_index = dataset.index.get_loc( name )
+        distances, indices = knn_model.kneighbors( dataset.loc[name, :].values.reshape( 1, -1 ),
+                                                   n_neighbors=11 )
+
+        recommendations = list()
+        for i in range( 0, len( distances.flatten() ) ):
+            if i == 0:
+                print( 'Recommendations for {0}:\n'.format( dataset.index[query_index] ) )
+            else:
+                print( '{0}: {1}, with distance of {2}:'.format( i, dataset.index[indices.flatten()[i]],
+                                                                 distances.flatten()[i] ) )
+                recommendations.append( dataset.index[indices.flatten()[i]] )
+        return HttpResponse( "||".join(recommendations ))
+    else:
+        return HttpResponse( 'it is not a POST request' )
