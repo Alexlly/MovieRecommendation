3f6b7031cc7a46f8ea59ba023526c9c3526a9888
CalvinDong <calvin.dong156@gmail.com>
2022-05-16 22:32:49 +1000

    - connected to the api

--- frontend/src/components/MovieCards.vue
+++ frontend/src/components/MovieCards.vue
@@ -1,14 +1,14 @@
 <template>
   <n-card :title=name>
     <template #cover>
-      <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
+      <img src="https://m.media-amazon.com/images/M/MV5BMjA5NjM3NTk1M15BMl5BanBnXkFtZTgwMzg1MzU2NjE@._V1_SX300.jpg">
     </template>
-    {{this.name}}
   </n-card>
 </template>
 
 <script>
 import { NCard } from "naive-ui"
+import axios from "axios"
 
 export default {
   name: 'MovieCard',
@@ -23,8 +23,45 @@ export default {
     }
   },
 
+  methods: {
+    getMovie(){
+
+    },
+
+    findFromQuery(){
+      let test = [
+        { Title: "Sicario", Year: "2015", imdbID: "tt3397884", Poster: "https://m.media-amazon.com/images/M/MV5BMjA5NjM3NTk1M15BMl5BanBnXkFtZTgwMzg1MzU2NjE@._V1_SX300.jpg"},
+        { Title: "Sicario: Day of the Soldado", Year: "2018", imdbID: "tt5052474", Poster: "https://m.media-amazon.com/images/M/MV5BMTEzNjM2NTYxMjReQTJeQWpwZ15BbWU4MDU2NzAxNTUz._V1_SX300.jpg"} ,
+        { Title: "American Sicario", Year: "2021", imdbID: "tt13322120", Poster: "https://m.media-amazon.com/images/M/MV5BYzU2NDk0MTEtYjE3Yi00…ItYjg2NDkwOWZmY2I3XkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_SX300.jpg" },
+      ] // Pretending to be data from the Movie Database Alternative API
+      for (let i = 0; i < test.size; i++){
+        console.log("testt")
+      }
+    }
+  },
+
   props: {
     name: String,
+  },
+
+  created(){
+    const key = process.env.MOVIE_KEY
+    console.log(key)
+    const options = {
+      method: 'GET',
+      url: 'https://movie-database-alternative.p.rapidapi.com/',
+      params: {s: 'Sicario', r: 'json', page: '1'},
+      headers: {
+        'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
+        'X-RapidAPI-Key': `${process.env.VUE_APP_MOVIE_KEY}`
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
   }
 }
 </script>
